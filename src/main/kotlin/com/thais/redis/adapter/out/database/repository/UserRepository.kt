837e59fcbef0af8a5a67be61b24c8e6dcfd34d1c
Thais Almeida <Thaiscarolyne_itba@hotmail.com>
2021-03-28 22:21:57 -0300

    [FIX] Optimizing imports

--- src/main/kotlin/com/thais/redis/adapter/out/database/repository/UserRepository.kt
+++ src/main/kotlin/com/thais/redis/adapter/out/database/repository/UserRepository.kt
@@ -1,10 +1,7 @@
 package com.thais.redis.adapter.out.database.repository
 
-import com.thais.redis.adapter.out.database.postgres.entity.UserEntity
 import com.thais.redis.application.domain.User
 import com.thais.redis.application.ports.out.UserRepositoryPort
-import io.micronaut.data.annotation.Repository
-import io.micronaut.data.repository.CrudRepository
 
 class UserRepository: UserRepositoryPort{
     override fun getUserById(id: Long): User {
